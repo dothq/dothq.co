651a3d5e68442d8593e405607cc96fff476118d0
EnderDev <xenderpugx@gmail.com>
2020-04-15 16:49:32 +0100

    ⚡ Add missing React import

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,7 +29,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      {window.location.host === "alpha.dothq.co" && <AlphaNotice />}
+      <AlphaNotice />
       <Header siteTitle={data.site.siteMetadata.title} />
       <Hero>
         {children}
