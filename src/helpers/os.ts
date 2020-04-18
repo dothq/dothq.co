63a6731e5713395761dd7ed9c675ff6f972b0340
EnderDev <xenderpugx@gmail.com>
2020-04-18 23:57:51 +0100

    ✨ Fix OS being missing on macOS

--- src/helpers/os.ts
+++ src/helpers/os.ts
@@ -1,6 +1,8 @@
 export const getOS = () => {
     if (typeof navigator !== 'undefined') {
-        const os = ['Windows', 'Linux', 'macOS'];
-        return os.find(v => navigator.appVersion.indexOf(v) >= 0);
+        const os = ['Windows', 'Linux', 'Mac OS'];
+        const detectedOS = os.find(v => navigator.appVersion.indexOf(v) >= 0);
+        if(detectedOS == "Mac OS") return "macOS"
+        return detectedOS
     }
 }
