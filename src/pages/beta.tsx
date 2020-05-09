5ecbb6057846e9e010ffa0eefa5251b41487fd84
EnderDev <xenderpugx@gmail.com>
2020-05-09 22:54:07 +0100

    📦 Add beta and download pages

--- src/pages/beta.tsx
+++ src/pages/beta.tsx
@@ -3,73 +3,66 @@ import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { HeroButton } from "../components/Button";
+import { HeroButton, TextButton } from "../components/Button";
 import { Buttons } from "../components/style";
 import FeatherIcon from 'feather-icons-react'
+import { Time } from "../components/BlogCard";
+import { getOS } from "../helpers/os";
+import { isBrowser } from "../helpers/login";
+import Emoji from "react-emoji-render";
+import { generateEmojiConfig } from "../tools/emoji";
 
-// const launchDate = 1587466800000;
-const launchDate = 1587466800000;
+//1589108400000
+const launchDate = -1;
+				   
+const BetaPage = () => {
+  const builds = [
+	{
+		id: 0,
+		version: "3.0.0-alpha",
+		product: "Dot Browser",
+		releasesAt: 1589108400000,
+		released: ((1589108400000 - new Date().getTime()) / 1000) < 1,
+		download: "https://dothq.co/api/api.beta?folder=3.0.0-alpha&file=Install%20Dot%20Browser-0.0.1-alpha.exe"
+	}
+  ]
 
-const SecondPage = () => {
-	const [timeUntilLaunch, setTimeUntilLaunch] = React.useState();
+  const download = (id) => {
+	  const build = builds.find(build => build.id == id);
 
-	React.useEffect(() => {
-		const showTime = () => {
-			var t = (launchDate - new Date().getTime()) / 1000;
-			if (t > 1) {
-				const days = Math.floor(t / 86400);
-				t -= days * 86400;
-				const hours = Math.floor(t / 3600) % 24;
-				t -= hours * 3600;
-				const minutes = Math.floor(t / 60) % 60;
-				t -= minutes * 60;
-				const seconds = Math.floor(t % 60);
-
-				setTimeUntilLaunch((
-					`${days} ${days === 1 ? "day" : "days"}, ${hours} ${
-						hours === 1 ? "hour" : "hours"
-					}, ${minutes} ${
-						minutes === 1 ? "minute" : "minutes"
-					} and ${seconds} ${seconds === 1 ? "second" : "seconds"}`
-        ) as any);
-			} else {
-				setTimeUntilLaunch(("-" as any));
-			}
-		};
-		const countdown = setInterval(showTime, 1000);
-		showTime();
-
-		return () => {
-			clearInterval(countdown);
-		};
-	}, []);
+	  fetch(build.download)
+		  .then(res => res.json())
+		  .then(res => {
+			  if(res.ok == true) {
+				const url = decodeURIComponent(Buffer.from(res.download, "base64").toString('utf-8'))
+				isBrowser() ? window.location.href = url : ''
+			  }
+		  })
+  }
 
   return (
-    <Layout>
-      <SEO title="Dot Browser Beta Testing" />
-      {timeUntilLaunch !== "-" && <CountdownDisplay timeUntilLaunch={timeUntilLaunch} />}
-      {timeUntilLaunch == "-" && <LaunchDisplay />}
+	<Layout>
+	  <SEO title="Beta" />
+	  <Emoji text={"📦"} options={generateEmojiConfig({ className: 'emoji' })} />
+      <h1 style={{ fontSize: '80px' }}>Beta Builds</h1>
+      <p>Available builds for download</p>
+      <table style={{ maxWidth: '1000px', margin: '0 auto' }}>
+		<tr>
+			<th>Version</th>
+			<th>Product</th>
+			<th>Build release</th>
+			<th>Download</th>
+		</tr>
+		{builds.map(build => (
+			<tr key={build.id}>
+				<td>{build.version}</td>
+				<td>{build.product}</td>
+				<td><Time date={build.releasesAt} /></td>
+				<td><HeroButton onClick={() => download(build.id)} shade={"blue"} style={{ width: 'max-content', height: '42px', opacity: build.released ? 1 : "0.5", userSelect: build.released ? "all" : "none", pointerEvents: build.released ? "all": "none" }}><FeatherIcon icon="download" size={18} style={{ marginRight: '8px' }} /> Download for {getOS()}</HeroButton></td>
+			</tr>
+		))}
+	  </table>
     </Layout>
 )}
 
-export default SecondPage
-
-const CountdownDisplay = ({ timeUntilLaunch }) => {
-  return (
-    <>
-      <h1 style={{ fontSize: '80px' }}>Coming soon</h1>
-      <p>Beta builds unlocks 21th April 2020 at {new Date(1587466800000).toLocaleString().split(", ")[1].split(":")[0]}:00 {/\((.*)\)/.exec(new Date().toString())[1].replace(/[a-z]/g, "").replace(/ /g, "")}.</p>
-      <p>{timeUntilLaunch} to go</p>
-    </>
-)}
-
-const LaunchDisplay = () => {
-  return (
-    <>
-      <h1 style={{ fontSize: '80px' }}>Beta Launched</h1>
-      <p>Press the redeem button below to receive a beta version of Dot Browser.</p>
-      <Buttons>
-        <HeroButton shade={"blue"}><FeatherIcon icon="gift" size={18} style={{ marginRight: '8px' }} /> Redeem build</HeroButton>
-      </Buttons>
-    </>
-)}
+export default BetaPage;
