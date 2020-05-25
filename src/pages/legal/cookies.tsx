6e06e9b0d30137634bebc436bbe30fd679057edd
EnderDev <xenderpugx@gmail.com>
2020-05-25 19:28:38 +0100

    ✨ Give the cookies page some content

---
+++ src/pages/legal/cookies.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const CookiesPage = () => {
+    return (
+        <Layout>
+            <SEO title="" />
+            <div>
+                Work in progress
+            </div>
+        </Layout>
+    )
+}
+
+export default CookiesPage
