6ffb1908e402b608368ce3a0df99cf7320745721
EnderDev <xenderpugx@gmail.com>
2020-05-25 19:27:09 +0100

    ✨ Give the terms and gdpr page some content

---
+++ src/pages/legal/gdpr.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const GDPRPage = () => {
+    return (
+        <Layout>
+            <SEO title="" />
+            <div>
+                Work in progress
+            </div>
+        </Layout>
+    )
+}
+
+export default GDPRPage
---
+++ src/pages/legal/terms.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+
+const TermsPage = () => {
+    return (
+        <Layout>
+            <SEO title="" />
+            <div>
+                Work in progress
+            </div>
+        </Layout>
+    )
+}
+
+export default TermsPage
