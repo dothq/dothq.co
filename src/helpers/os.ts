5d0300b3f3a9c6c58ddcfff52ed57fa26eff15f3
Kieran <xenderpugx@gmail.com>
2020-06-04 13:55:46 +0100

    🐛 Fix the order in which we detect the OS

--- src/helpers/os.ts
+++ src/helpers/os.ts
@@ -1,6 +1,6 @@
 export const getOS = () => {
     if (typeof navigator !== 'undefined') {
-        const os = ['Android', 'iPhone', 'Windows', 'Linux', 'Mac OS'];
+        const os = ['Android', 'iPhone', 'Windows', 'Mac OS', 'Linux'];
         const detectedOS = os.find(v => navigator.appVersion.indexOf(v) >= 0);
         if(detectedOS == "Mac OS") return "macOS"
         if(detectedOS == "iPhone") return "iOS"
