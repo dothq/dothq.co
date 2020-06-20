56ccacf2e744db25e91b1469950c1823f514ac21
Jake Ward <geek@gaminggeek.dev>
2020-06-20 22:03:40 +0100

    s l a s h

--- src/helpers/me.ts
+++ src/helpers/me.ts
@@ -1,7 +1,7 @@
 import { getUserToken } from "./login";
 
 export const getMe = async () => {
-    const res = await fetch('https://dothq.co/api/id.me', { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + getUserToken() } });
+    const res = await fetch('https://dothq.co/api/id/me', { headers: { "Content-Type": "application/json", "Authorization": "Bearer " + getUserToken() } });
 
     return res.json();
 }
