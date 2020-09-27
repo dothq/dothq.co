b0ff8b9158fbc17d2752e7e46c0e05a7268bfb05
EnderDev <xenderpugx@gmail.com>
2020-09-27 14:51:17 +0100

    ✨ Move the legal documents to markdown

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,11 @@
+const axios = require("axios")
 const path = require(`path`)
 const { paginate } = require(`gatsby-awesome-pagination`)
+const { resolve } = require("path")
+
+exports.onCreateNode = ({ node }) => {
+    console.log(`Node created of type "${node.internal.type}"`)
+}
 
 /**
  * Here is the place where Gatsby creates the URLs for all the
@@ -108,5 +114,73 @@ exports.createPages = async ({ graphql, actions }) => {
                 return `/blog`
             }
         },
+    })
+
+    const getMarkdown = (url) => {
+        return new Promise((resolve) => {
+            axios.get(url)
+                .then(res => {
+                    console.log(res.headers)
+                    resolve(res.data)
+                })
+        })
+    }
+
+    const getMarkdownMeta = (file) => {
+        return new Promise((resolve) => {
+            axios.get(`https://api.github.com/repos/dothq/legal/commits?path=${file}&page=1&per_page=1`)
+                .then(res => {
+                    const months = {
+                        0: "January",
+                        1: "February",
+                        2: "March",
+                        3: "April",
+                        4: "May",
+                        5: "June",
+                        6: "July",
+                        7: "August",
+                        8: "September",
+                        9: "October",
+                        10: "November",
+                        11: "December"
+                    }
+
+                    const d = new Date(res.data[0].commit.committer.date);
+                    const date = `${d.getDate()} ${months[d.getMonth()]} ${d.getFullYear()}`;
+
+                    resolve({
+                        lastUpdated: date,
+                        author: {
+                            avatar: res.data[0].author.avatar_url,
+                            name: res.data[0].author.login,
+                        }
+                    })
+                })
+        })
+        
+    }
+
+    const generateLegalContext = async (policyName) => {
+        const base = "https://raw.githubusercontent.com/dothq/legal/master"
+
+        const policy = await getMarkdown(`${base}/${policyName}.md`);
+        const policyMeta = await getMarkdownMeta(`${policyName}.md`);
+
+        const names = {
+            PRIVACY_POLICY: "Privacy Policy"
+        }
+
+        return {
+            title: names[policyName],
+            content: policy,
+            historyLink: `https://github.com/dothq/legal/commits/master/${policyName}.md`,
+            ...policyMeta
+        }
+    }
+
+    createPage({
+        path: `/legal/privacy`,
+        component: require.resolve("./src/templates/legal.tsx"),
+        context: { legal: await generateLegalContext("PRIVACY_POLICY") },
     })
 }
